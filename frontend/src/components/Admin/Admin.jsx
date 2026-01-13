07f0b012d4a515177aa6198de60be36905aaf60b
kouki <kouki@DESKTOP-FQCBJ7K>
2026-01-13 08:32:18 +0100

    bi

--- frontend/src/components/Admin/Admin.jsx
+++ frontend/src/components/Admin/Admin.jsx
@@ -8,7 +8,7 @@ function Admin() {
           <i className="fas fa-chart-line"></i>
         </div>
         <h1>Espace Admin</h1>
-        <p>Les analyses BI seront disponibles bientôt.</p>
+        <iframe title="i wanna die" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiNzBhNWJjMjktZTg2OC00ZDkwLThkODEtODVhZjNhYTYyYzhhIiwidCI6ImI3YmQ0NzE1LTQyMTctNDhjNy05MTllLTJlYTk3ZjU5MmZhNyJ9" frameborder="0" allowFullScreen="true"></iframe>
       </div>
     </div>
   );
